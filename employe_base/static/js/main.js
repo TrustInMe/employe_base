bf97017a40d024b9d880bdd9fcab69da9c08244d
TrustInMe <andrej-vilenskij@yandex.ru>
2019-04-15 10:44:35 +0300

    little js/css fixes for small screens

--- employe_base/static/js/main.js
+++ employe_base/static/js/main.js
@@ -1,5 +1,16 @@
-// $(document).ready(function() {
-//     console.log('works');
+$(document).ready(function() {
+    
+    var url = window.location.href;
 
+    var workers = url.split('?').pop().split('&')[0].split('=')[1]
+    var departments = decodeURI(url.split('&').pop().split('=')[1]);
 
-// });
+    if (workers != null){
+        $('option[value="'+workers+'"]').attr('selected', true);
+    } 
+
+    if (departments != null){
+        $('option[value="'+departments+'"]').attr('selected', true);
+    } 
+
+});
